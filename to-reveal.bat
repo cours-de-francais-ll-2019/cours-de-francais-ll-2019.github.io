fbaf24557633fb58cb028d77d24cb825a6b19ea7
Alexandre Sobieski <nasmi3+github@gmail.com>
2019-05-04 00:22:55 +0900

    Possibilité refaire conversion avant déplacement

--- to-reveal.bat
+++ to-reveal.bat
@@ -4,12 +4,18 @@ echo   _                    _           ___  ___   _
 echo  ^|_) ._ _  o  _ _^|_   ^|_)    /\     ^|    ^|   / \   
 echo  ^|   ^| (_) ^| (/_ ^|_   ^|_) o /--\ o _^|_ o ^| o \_/ o 
 echo           _^|                                       
+:choixnumero
 echo.
 echo Conversion des fichiers du type lecon-XX.md
 set /P id="Numéro de leçon à convertir : "
+:conversion
 echo.
 pandoc -t revealjs -s --css="theme.css" -o lecons/lecon-%id%.html lecon-%id%.md --slide-level=2 -V revealjs-url=https://revealjs.com
 echo Conversion de la leçon %id% terminée
 echo.
-echo move "lecon-"%id%".md" "converti/lecon-"%id%".md"
+set /P ok="Le résultat est-il satisfaisant ? [O/n]"
+echo.
+if %ok%==n (goto conversion)
+if %ok%==N (goto conversion)
+move "lecon-"%id%".md" "converti/lecon-"%id%".md"
 echo Transfert du fichier lecon-%id%.md terminé
